c5a51c03c948c6be54e054cc464880befa69a2c1
yourfather <okuhlexesibe0@gmail.com>
2025-05-25 20:25:23 +0200

    CLI version of the smart shopper ..bare with me

---
+++ smartshopr/smartshopper/target/debug/deps/smartshopper-4f776f633f1694aa.d
@@ -0,0 +1,5 @@
+/home/okuhle/GULAG/smartshopr/smartshopper/target/debug/deps/libsmartshopper-4f776f633f1694aa.rmeta: src/main.rs
+
+/home/okuhle/GULAG/smartshopr/smartshopper/target/debug/deps/smartshopper-4f776f633f1694aa.d: src/main.rs
+
+src/main.rs:
